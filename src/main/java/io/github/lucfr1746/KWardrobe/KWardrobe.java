42e7792c194b90d831fb080a25c170bab1a5fe4a
Thế Lực <theluc.1746@gmail.com>
2025-07-22 15:51:10 +0700

    Add metrics and update checker to KWardrobe
    Integrated bStats metrics collection and an update checker into the KWardrobe plugin. Added new classes for metrics (bStatsMetrics) and update checking (UpdateChecker), and updated the main plugin class to initialize these features on startup.

--- src/main/java/io/github/lucfr1746/KWardrobe/KWardrobe.java
+++ src/main/java/io/github/lucfr1746/KWardrobe/KWardrobe.java
@@ -1,17 +1,27 @@
 package io.github.lucfr1746.KWardrobe;
 
+import io.github.lucfr1746.KWardrobe.Metrics.UpdateChecker;
+import io.github.lucfr1746.KWardrobe.Metrics.bStatsMetrics;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class KWardrobe extends JavaPlugin {
 
+    private static KWardrobe plugin;
+
     @Override
     public void onEnable() {
-        // Plugin startup logic
-
+        plugin = this;
+    // Metrics
+        new UpdateChecker(this);
+        new bStatsMetrics(this, 26382);
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+    }
+
+    public static Plugin getInstance() {
+        return plugin;
     }
 }
---
+++ src/main/java/io/github/lucfr1746/KWardrobe/Metrics/UpdateChecker.java
@@ -0,0 +1,46 @@
+package io.github.lucfr1746.KWardrobe.Metrics;
+
+import io.github.lucfr1746.KWardrobe.KWardrobe;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.Scanner;
+import java.util.function.Consumer;
+
+public class UpdateChecker {
+
+    private final KWardrobe plugin;
+
+    public UpdateChecker(KWardrobe plugin) {
+        this.plugin = plugin;
+        check(-1);
+    }
+
+    public void getLatestVersion(Consumer<String> consumer, int resourceId) {
+        try (InputStream inputStream = new URI("https://api.spigotmc.org/legacy/update.php?resource=" + resourceId).toURL().openStream();
+             Scanner scanner = new Scanner(inputStream)) {
+
+            if (scanner.hasNext()) {
+                consumer.accept(scanner.next());
+            }
+        } catch (IOException | URISyntaxException exception) {
+            plugin.getLogger().info("Update checker is broken, can't find an update! " + exception.getMessage());
+        }
+    }
+
+    public void check(int resourceId) {
+//        getLatestVersion(version -> {
+//            PluginDescriptionFile pdf = plugin.getDescription();
+//            // Check if the current version matches the latest version.
+//            if (pdf.getVersion().equalsIgnoreCase(version)) {
+//                this.plugin.getLogger().info("Version: " + ChatColor.GREEN + pdf.getVersion());
+//                this.plugin.getLogger().info("Plugin is up to date!");
+//            } else {
+//                this.plugin.getLogger().info("Version: " + ChatColor.RED + pdf.getVersion());
+//                this.plugin.getLogger().info("Plugin has an update! Download at: https://www.spigotmc.org/resources/" + resourceId);
+//            }
+//        }, resourceId);
+    }
+}
